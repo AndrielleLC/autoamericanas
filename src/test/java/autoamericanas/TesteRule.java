6b7afc0765cbc812fa223b8469835ca6d09c6a63
Andrielle Calegari <andrielle.calegari@hotmail.com>
2019-10-02 16:06:32 -0300

    Correção navegador padrão

--- src/test/java/autoamericanas/TesteRule.java
+++ src/test/java/autoamericanas/TesteRule.java
@@ -15,11 +15,11 @@ public class TesteRule {
     @Before
     public void beforeCenario(){
         //Chrome
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
-        driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
+        //driver = new ChromeDriver();
 
-        //System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver/geckodriver.exe");
-        //driver = new FirefoxDriver();
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver/geckodriver.exe");
+        driver = new FirefoxDriver();
 
         driver.manage().window().maximize();
         driver.navigate().to("https://www.americanas.com.br/");
